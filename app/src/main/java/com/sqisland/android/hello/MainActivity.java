2e88234ac1182e6fd893c1ce62c4aff5debe0025
nzns <37954949+nzns@users.noreply.github.com>
2026-03-23 21:32:22 +0800

    Update MainActivity.java

--- app/src/main/java/com/sqisland/android/hello/MainActivity.java
+++ app/src/main/java/com/sqisland/android/hello/MainActivity.java
@@ -1,12 +1,26 @@
-package com.sqisland.android.hello;
+package com.shortcut.doubao;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        try {
+            // 设置跳转链接
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("doubao://voice"));
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        } catch (Exception e) {
+            // 如果没安装豆包，防止应用崩溃
+            e.printStackTrace();
+        }
+        
+        // 启动目标后立即关闭当前空白 Activity
+        finish();
+    }
 }
